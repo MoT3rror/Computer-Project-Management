fce4ae6b1f8b3ad0c38559bca2a70b354621fb29
MoT3rror <joshbixler@sbcglobal.net>
2013-12-14 19:22:51 -0600

    Update connections.php
    Removed password and put in blank for default

--- app/config/bootstrap/connections.php
+++ app/config/bootstrap/connections.php
@@ -64,7 +64,7 @@ use lithium\data\Connections;
  	'adapter' => 'MySql',
  	'host' => 'localhost',
  	'login' => 'root',
- 	'password' => 'Pa$$W0rd',
+ 	'password' => '',
  	'database' => 'projectmanagement',
  	'encoding' => 'UTF-8'
  ));
